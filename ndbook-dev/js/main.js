f6afadff1755894235dffb09f1fa2d1a4f8126e5
mkess-ivy <montier.kess@gmail.com>
2016-07-20 01:12:14 -0400

    added smooth scroll

--- ndbook-dev/js/main.js
+++ ndbook-dev/js/main.js
@@ -32,5 +32,17 @@ $(document).ready(function(){
   //Call it on resize.
   }).resize();
 
+    // Smooth Scroll
+    $('a[href^="#"]').on('click',function (e) {
+      e.preventDefault();
 
+      var target = this.hash;
+      var $target = $(target);
+
+      $('html, body').stop().animate({
+          'scrollTop': $target.offset().top
+      }, 900, 'swing', function () {
+          window.location.hash = target;
+      });
+  });
 });
